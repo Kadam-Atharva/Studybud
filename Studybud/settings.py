320ee97dadcb60e3d634fc0040fa4dcb3d4cffaa
Atharva Kadam <atharvakadam751@gmail.com>
2025-06-05 18:36:22 +0530

    Second

--- Studybud/settings.py
+++ Studybud/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-8!q($ix#2fu)r%2(8j9bk%br^7vudbt_0erafx^uquzoz1!ysb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['studybud-dpm6.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
