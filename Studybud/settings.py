5fe3efc1f4497196e36aeaff2c69cc10e84d0dcb
Atharva Kadam <atharvakadam751@gmail.com>
2025-07-08 19:22:15 +0530

    Second

--- Studybud/settings.py
+++ Studybud/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8!q($ix#2fu)r%2(8j9bk%br^7vudbt_0erafx^uquzoz1!ysb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['studybud-vm6w.onrender.com']
 
